3502aaeacd389ca5ffe875e5182fb0e8efeb097b
mikhail <mike.42.alexeev@gmail.com>
2021-02-22 14:20:42 +0300

    use logging

--- ycrawler.py
+++ ycrawler.py
@@ -1,8 +1,9 @@
 import asyncio
 import hashlib
+import logging
 import os
-import time
 import sys
+import time
 
 import aiohttp
 import aiofiles
@@ -14,21 +15,27 @@ BASE_URL = 'https://news.ycombinator.com'
 SLEEP_SECONDS = 5
 MAX_REQUESTS = 5
 
+logging.basicConfig(level=logging.INFO)
+
+
+def get_logger():
+    return logging.getLogger()
+
 
 async def get_page_content(url, semaphore):
     await semaphore.acquire()
-    print(f'do request to {url}')
+    get_logger().info(f'do request to {url}')
     try:
         async with aiohttp.ClientSession() as session:
             resp = await session.request('GET', url)
             return await resp.read()
     finally:
-        print('semaphore released')
+        get_logger().info('semaphore released')
         semaphore.release()
 
 
 async def save_page_content(url, output_folder, semaphore):
-    print(f'save page {url}')
+    get_logger().info(f'save page {url}')
     content = await get_page_content(url, semaphore)
     hash_ = hashlib.sha256(url.encode()).hexdigest()
     output_path = os.path.join(output_folder, hash_)
@@ -37,11 +44,11 @@ async def save_page_content(url, output_folder, semaphore):
 
 
 async def save_pages_content_from_comments(url, output_folder, semaphore):
-    print(f'save from comments {url}')
+    get_logger().info(f'save from comments {url}')
     comments_content = await get_page_content(url, semaphore)
     external_links = parse_comments_page(comments_content)
     if external_links:
-        print('external_links', external_links)
+        get_logger().info(f'external_links: {external_links}')
         todo = [save_page_content(url_, output_folder, semaphore) for url_ in external_links]
         await asyncio.wait(todo)
 
@@ -78,7 +85,6 @@ def parse_main_page(page_content):
 
 
 async def create_folders(output_folder, news_ids):
-    print('create folders', output_folder, news_ids)
     todo = [aiofiles.os.mkdir(os.path.join(output_folder, str(id_)))
             for id_ in news_ids]
 
@@ -92,7 +98,7 @@ async def do_requests(already_seen_news, output_folder):
                           if id_ not in already_seen_news}
 
     if not news_ids_with_urls:
-        print('no new news')
+        get_logger().info('no new news')
         return
 
     await create_folders(output_folder, news_ids_with_urls.keys())
